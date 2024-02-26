d9a3e620a380f0c1887daff05b2d6935e1feb7ab
Alefe06 <alefevigatto123@gmail.com>
2024-02-26 08:39:44 -0300

    att

---
+++ App.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Switch,ImageBackground, ScrollView } from 'react-native';
+
+const  icon = require('./assets/icon.png');
+const backgroundImage = require('./assets/background.jpg');
+
+export default function App() {
+const [habilitado, setHabilitado] = new useState(false);
+
+const habilitar = () => {
+  setHabilitado(!habilitado);
+}
+
+  return (
+    <ImageBackground source={backgroundImage} style={styles.background}>
+    <View style={styles.container}>
+      <Image
+      source={icon}
+      style={styles.icon}
+      />
+      <Switch
+      value={habilitado}
+      onValueChange={habilitar}
+      />
+      <Image
+      source={{
+        uri: (habilitado)
+        ? "./burro.jpg"
+        : "./assets/eusabo.jpg"
+      }}
+      style={[styles.lampada, {opacity: 0.8}]}
+      />
+      <Image
+      source={{
+        uri: "https://w7.pngwing.com/pngs/924/553/png-transparent-thirty-one-gift-idea-business-organization-plan-man-service-sticker-musician.png"
+
+      }}
+      style={styles.pessoa}
+      />
+      <StatusBar style="auto" />
+    </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    
+  },
+  container: {
+    flex: 1,
+    backgroundColor:'rgba(255, 255, 255, 0.5)', // Adicionando transparência ao contêiner
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '32px'
+  },
+  icon: {
+    width: '68px',
+    height: '68px'
+  },
+  lampada: {
+    width: '80px',
+    height: '80px'
+  },
+  pessoa: {
+    width: '200px',
+    height: '200px'
+  }
+});
